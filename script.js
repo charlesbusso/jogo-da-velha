8f8027d4a4662053d9890350bd5ea2a0c9d5dc3f
charles busso <charlesagbusso13@gmail.com>
2023-02-03 18:42:54 -0300

    Ok

--- script.js
+++ script.js
@@ -52,11 +52,13 @@ function cpuJoga(){
                     jogo[2][1] = "O";
                 } else if((jogo[0][1] == "O")&&(jogo[2][1] == "O")&&(jogo[1][1] == "")){
                     jogo[1][1] = "O";
+                }else if((jogo[1][1] == "O")&&(jogo[2][1] == "O")&&(jogo[0][1] == "")){
+                    jogo[0][1] = "O";
                 }//coluna 3
                 else if((jogo[0][2] == "O")&&(jogo[1][2] == "O")&&(jogo[2][2] == "")){
                     jogo[2][2] = "O";
-                } else if((jogo[0][2] == "O")&&(jogo[2][2] == "O")&&(jogo[2][0] == "")){
-                    jogo[2][0] = "O";
+                } else if((jogo[0][2] == "O")&&(jogo[2][2] == "O")&&(jogo[1][2] == "")){
+                    jogo[1][2] = "O";
                 } else if((jogo[1][2] == "O")&&(jogo[2][2] == "O")&&(jogo[0][2] == "")){
                     jogo[0][2] = "O";
                 }//jogada diagonal 1
